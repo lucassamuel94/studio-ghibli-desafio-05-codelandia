533805b0d642f557ee52052af416a72569a3babe
Lucas Samuel <lucassamuel.dev@gmail.com>
2023-12-22 10:40:12 -0300

    config container

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -11,6 +11,19 @@ const config:Config = {
         'background': "url('src/assets/background.png')"
       }
     },
+    container: {
+      center: true,
+      padding: {
+        DEFAULT: '1.5rem',
+        md: '2rem'
+      },
+      screens: {
+        'sm': '640px',
+        'md': '768px',
+        'lg': '1024px',
+        'xl': '1280px',
+      }
+    },
     fontFamily: {
       sans: "'Archivo', sans-serif",
       heading: "'Andada Pro', serif"
